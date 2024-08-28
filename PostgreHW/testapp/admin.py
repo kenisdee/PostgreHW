b9e0dba5b739e6f1ee902d5639d6352cec9915b2
kenisdee <kdenis112@gmail.com>
2024-08-28 10:33:48 +0400

    create admin.py

--- PostgreHW/testapp/admin.py
+++ PostgreHW/testapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Buyer, Game
 
-# Register your models here.
+# Регистрация модели Buyer
+@admin.register(Buyer)
+class BuyerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'balance', 'age')  # Поля, которые будут отображаться в списке
+    search_fields = ('name',)  # Поля, по которым можно осуществлять поиск
+    list_filter = ('age',)  # Поля, по которым можно фильтровать
+
+# Регистрация модели Game
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('title', 'cost', 'size', 'age_limited')  # Поля, которые будут отображаться в списке
+    search_fields = ('title', 'description')  # Поля, по которым можно осуществлять поиск
+    list_filter = ('age_limited',)  # Поля, по которым можно фильтровать
